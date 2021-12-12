c9c33b773b81afee1c11847d2d3123e37433e1a4
Barnabas Molnar <barnabas.molnar@gmgcolor.com>
2021-12-12 19:55:41 +0100

    Css adjustments Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
-import './Header.css'
-import SearchIcon from '@mui/icons-material/Search';
+import './Header.css';
 import HeaderOption from './HeaderOption';
-import HomeIcon from '@mui/icons-material/Home';
-import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
-import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
-import ChatIcon from '@mui/icons-material/Chat';
-import NotificationsIcon from '@mui/icons-material/Notifications';
+import { LaptopChromebook, Search, Apps, Home, SupervisorAccount, BusinessCenter, Chat, Notifications } from '@mui/icons-material';
 import { useDispatch } from 'react-redux';
 import { logout } from '../features/userSlice';
 import { auth } from '../firebase';
@@ -16,29 +11,33 @@ import { auth } from '../firebase';
 const Header = () => {
 
 
-    const dispatch = useDispatch()
-    const logoutFromApp = ()=> {
-        dispatch(logout())
-        auth.signOut()
-    }
-    
+    const dispatch = useDispatch();
+    const logoutFromApp = () => {
+        dispatch(logout());
+        auth.signOut();
+    };
+
     return (
         <div className='header'>
+            <div className="header__flex">
 
-            <div className="header__left">
-                <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="" />
-                <div className="header__search">
-                    <SearchIcon />
-                    <input type="text" placeholder='Search'/>
+                <div className="header__left">
+                    <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="" />
+                    <div className="header__search">
+                        <Search />
+                        <input type="text" placeholder='Search' />
+                    </div>
                 </div>
-            </div>
-            <div className="header__right">
-            <HeaderOption Icon={HomeIcon} title='Home'/>
-            <HeaderOption  Icon={SupervisorAccountIcon} title='My network'/>
-            <HeaderOption  Icon={BusinessCenterIcon}  title='Jobs'/>
-            <HeaderOption   Icon={ChatIcon} title='Messagging'/>
-            <HeaderOption   Icon={NotificationsIcon} title='Notifications'/>
-            <HeaderOption   title='me' avatar onClick={logoutFromApp}/>
+                <div className="header__right">
+                    <HeaderOption Icon={Home} title='Home' />
+                    <HeaderOption Icon={SupervisorAccount} title='My network' />
+                    <HeaderOption Icon={BusinessCenter} title='Jobs' />
+                    <HeaderOption Icon={Chat} title='Messagging' />
+                    <HeaderOption Icon={Notifications} title='Notifications' />
+                    <HeaderOption title='me' avatar onClick={logoutFromApp} />
+                    <HeaderOption title='Work' Icon={Apps} />
+                    <HeaderOption title='Learning' Icon={LaptopChromebook} />
+                </div>
             </div>
         </div>
     );
--- src/Components/HeaderOption.jsx
+++ src/Components/HeaderOption.jsx
@@ -8,12 +8,14 @@ const HeaderOption = ({title, Icon, avatar, onClick}) => {
 
     const user = useSelector(selectUser)
     return (
-        <div className='headerOption' onClick={onClick}>
-            {Icon && < Icon className='headerOption__icon' />}
-            {avatar && (
-                <Avatar className='headerOption__icon' src={user?.photUrl} >{user?.displayName[0]}</Avatar> 
-            )}
-            <h3 className='headerOption__title'>{user?.displayName || title}</h3>
+        <div className={`headerOption__wrapper ${avatar ? "rightBorder-weak" : ''}`}>
+            <div className='headerOption' onClick={onClick}>
+                {Icon && < Icon className='headerOption__icon' />}
+                {avatar && (
+                    <Avatar className='headerOption__icon' src={user?.photUrl} >{user?.displayName[0]}</Avatar> 
+                    )}
+                <h3 className='headerOption__title'>{avatar ?user?.displayName : title}</h3>
+            </div>
         </div>
     )
 }
