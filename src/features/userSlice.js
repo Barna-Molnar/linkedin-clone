8b62c3c3ba0d92a20a216e8d00ba8d64bb16c419
Barnabas Molnar <barnabas.molnar@gmgcolor.com>
2021-12-26 15:54:13 +0100

    cleaning

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -23,19 +23,3 @@ export const { login, logout } = userSlice.actions;
 export const selectUser = (state) => state.user.user;
 
 export default userSlice.reducer;
-
-export const setProgressBar = (isOpen) => (
-  {
-   type: 'SET_PROGRESS_BAR',
-   isOpen: isOpen
-  });
-
-  export const ui = (state = {}, action) => {
-    switch (action.type) {
-      case 'SET_PROGRESS_BAR':
-        return Object.assign({}, state, { progressBarStatus: action.isOpen });
-  
-    default:
-     return state;
-   }
-  };
